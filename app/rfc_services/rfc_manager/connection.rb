6d1c221e34be6650ca7487065f95c51fc4fe4c80
Yury Matusevich <yury.a.matusevich@gmail.com>
2020-01-24 14:19:06 +0300

    add rfc services from gem

---
+++ app/rfc_services/rfc_manager/connection.rb
@@ -0,0 +1,25 @@
+class RFCManager::Connection < SimpleDelegator
+  CLOSE_EXCEPTION = 'CONNECTION CLOSE EXCEPTION'.freeze
+
+  def function(name)
+    discover(name).new_function_call
+  end
+
+  def close
+    __getobj__.close
+  rescue Exception => exc
+    Rails.logger.tagged(CLOSE_EXCEPTION) do
+      log_error(exc)
+    end
+  end
+
+  private
+
+  def log_error(exc)
+    Rails.logger.error(exc.message)
+
+    if exc.respond_to?(:error)
+      Rails.logger.error { exc.error.inspect }
+    end
+  end
+end
