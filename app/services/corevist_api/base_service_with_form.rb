5710f96f991cbbdd66150b6e91c0079b942e3689
Yury Matusevich <yury.a.matusevich@gmail.com>
2020-01-30 14:06:06 +0300

    fix mailer to work with sidekiq

--- app/services/corevist_api/base_service_with_form.rb
+++ app/services/corevist_api/base_service_with_form.rb
@@ -17,5 +17,9 @@ module CorevistAPI
     def perform
       raise NotImplementedError
     end
+
+    def to_json
+
+    end
   end
 end
--- app/services/corevist_api/services/user/registration_service.rb
+++ app/services/corevist_api/services/user/registration_service.rb
@@ -1,8 +1,8 @@
 module CorevistAPI
   class Services::User::RegistrationService < CorevistAPI::Services::BaseServiceWithForm
     def perform
-      CorevistAPI::Mailer.user_registration(@form).deliver_later
-      CorevistAPI::Admin::Mailer.user_registration(@form).deliver_later
+      CorevistAPI::Mailer.user_registration(@form.to_json).deliver_later
+      CorevistAPI::Admin::Mailer.user_registration(@form.to_json).deliver_later
     end
   end
 end
