75166b699889232b1760a6fa457d88b5e8c2e31e
Yury Matusevich <yury.a.matusevich@gmail.com>
2020-01-30 13:30:02 +0300

    configure mailer to use sidekiq

--- app/services/corevist_api/services/user/registration_service.rb
+++ app/services/corevist_api/services/user/registration_service.rb
@@ -1,8 +1,8 @@
 module CorevistAPI
   class Services::User::RegistrationService < CorevistAPI::Services::BaseServiceWithForm
     def perform
-      CorevistAPI::Mailer.user_registration(@form).deliver_now
-      CorevistAPI::Admin::Mailer.user_registration(@form).deliver_now
+      CorevistAPI::Mailer.user_registration(@form).deliver_later
+      CorevistAPI::Admin::Mailer.user_registration(@form).deliver_later
     end
   end
 end
