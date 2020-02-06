ea4d606d5b2271a0865487445260b68a66ea14b6
Yury Matusevich <yury.a.matusevich@gmail.com>
2020-02-06 18:15:11 +0300

    add valid url for reset password

--- app/mailers/corevist_api/devise_mailer.rb
+++ app/mailers/corevist_api/devise_mailer.rb
@@ -1,4 +1,11 @@
 module CorevistAPI
   class DeviseMailer < Devise::Mailer
+    helper CorevistAPI::DeviseMailerHelper
+
+    def template_paths
+      paths = super
+      paths.insert(2, 'corevist_api/mailer/devise_mailer')
+      paths
+    end
   end
 end
