4927bc1b4610045261d5f657501f7b59d7131ab0
Yury Matusevich <yury.a.matusevich@gmail.com>
2020-02-07 16:52:29 +0300

    fix redis configuration

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://127.0.0.1:6379/12' }
+  config.redis = { url: 'redis://172.16.100.11:6408/12' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://127.0.0.1:6379/12' }
+  config.redis = { url: 'redis://172.16.100.11:6408/12' }
 end
