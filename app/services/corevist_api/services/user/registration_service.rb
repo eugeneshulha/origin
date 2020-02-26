bd5d424521d05a6d31675d9a0ecf867f976e8c4b
Yury Matusevich <yury.a.matusevich@gmail.com>
2020-02-26 14:22:15 +0300

    fix registration service

--- app/services/corevist_api/services/user/registration_service.rb
+++ app/services/corevist_api/services/user/registration_service.rb
@@ -3,6 +3,8 @@ module CorevistAPI
     def perform
       CorevistAPI::Mailer.user_registration(@form.to_json).deliver_later
       CorevistAPI::Admin::Mailer.user_registration(@form.to_json).deliver_later
+
+      result
     end
   end
 end
