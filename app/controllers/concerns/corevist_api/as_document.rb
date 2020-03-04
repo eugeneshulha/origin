333462e01baa699a41dc2cee28611eb7ca145f9b
Yury Matusevich <yury.a.matusevich@gmail.com>
2020-03-05 00:55:44 +0300

    fix salesdoc display webservice

--- app/controllers/concerns/corevist_api/as_document.rb
+++ app/controllers/concerns/corevist_api/as_document.rb
@@ -7,13 +7,13 @@ module CorevistAPI
       before_action :dispatch_object
 
       def index
-        form = Factories::FormsFactory.instance.for(@obj.api_names[:list], params)
-        service = Factories::ServicesFactory.instance.for(@obj.api_names[:list], form)
+        form = form_for(@obj.api_names[:list], params)
+        service = service_for(@obj.api_names[:list], form)
         @result = service.call
       end
 
       def show
-        service = Factories::ServicesFactory.instance.for(@obj.api_names[:display], @obj, params)
+        service = service_for(@obj.api_names[:display], @obj, params)
         @result = service.call
       end
 
