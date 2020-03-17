6bea7ecff1ea3c5b4f41e5b0f4214df8e81f1cfd
Yury Matusevich <yury.a.matusevich@gmail.com>
2020-03-17 15:25:52 +0300

    add basic items search for invoices

--- app/services/corevist_api/services/invoice/sort_items_service.rb
+++ app/services/corevist_api/services/invoice/sort_items_service.rb
@@ -1,16 +1,21 @@
 module CorevistAPI
   module Services::Invoice
-    class SortItemsService < CorevistAPI::Services::BaseService
-
-      def call
-        perform!
-      end
+    class SortItemsService < CorevistAPI::Services::BaseServiceWithForm
 
       private
 
-      def perform!
-        @rfc_result = rfc_service_for(@object.api_names[:display], @object, @params).call
-        result(@rfc_result.data['items']&.as_json)
+      def perform
+        rfc_result = rfc_service_for(:invoice_display, @form, @params).call
+        items = rfc_result.data['items']
+        result(filter_by_query(items))
+      end
+
+      def filter_by_query(items)
+        return items if @params[:q].blank?
+
+        items.select do |item|
+          item.descr.downcase.include?(@params[:q]) || item.mat.downcase.include?(@params[:q])
+        end
       end
     end
   end
