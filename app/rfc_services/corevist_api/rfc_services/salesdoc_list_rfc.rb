3f86b097a6d1ef856c00a7303122bbd5c84cf8ff
Oleg Ryzhko <ryz.oleg@gmail.com>
2020-03-31 12:02:35 +0300

    [PHNX-116] add list salesdoc services

--- app/rfc_services/corevist_api/rfc_services/salesdoc_list_rfc.rb
+++ app/rfc_services/corevist_api/rfc_services/salesdoc_list_rfc.rb
@@ -1,11 +1,43 @@
 module CorevistAPI
   class RFCServices::SalesdocListRFC < CorevistAPI::RFCServices::BaseRFCService
+    private
+
+    def function_name
+      :salesdoc_list
+    end
+
     def input
-      super
+      rfc_object = object_to_rfc
+      rfc_user = user_to_rfc(user)
+      set_params(rfc_object.merge(rfc_user))
+    end
+
+    def object_to_rfc
+      {
+        SEARCH_CRITERIA => {
+          WITH_DESCRIPTIONS => TRUE_VAL,
+          MAX_RESULTS => '12000',
+          ITEM_LIST => FALSE_VAL,
+          TA_GROUP => '2'
+        }
+      }.tap do |hash|
+
+        hash[SEARCH_CRITERIA][SEARCH_OPTION] = @params[:search_option].to_s if @params[:search_option].present?
+        hash[SEARCH_CRITERIA][SOLD_TO_NR] = @params[:sold_to_nr].add_leading_zeros if @params[:sold_to_nr].present?
+        hash[SEARCH_CRITERIA][MAT] = @params[:material].to_s if @params[:material].present?
+        hash[SEARCH_CRITERIA][FROM_DOC_DATE] = @params[:from_date].to_s if @params[:from_date].present?
+        hash[SEARCH_CRITERIA][TO_DOC_DATE] = @params[:to_date].to_s if @params[:to_date].present?
+        hash[SEARCH_CRITERIA][PO_NR] = @params[:po_number].to_s if @params[:po_number].present?
+        hash[SEARCH_CRITERIA][SALESDOC_NR] = @params[:salesdoc_number].add_leading_zeros if @params[:salesdoc_number].present?
+      end
     end
 
     def output
       super
+
+      @data = get_function_param(SALES_DOCUMENTS).map do |invoice|
+        RfcResultEntry.new(self.class.name.demodulize.underscore, invoice)
+      end
     end
   end
 end
--- app/services/corevist_api/services/salesdoc/list_service.rb
+++ app/services/corevist_api/services/salesdoc/list_service.rb
@@ -1,7 +1,24 @@
 module CorevistAPI
   module Services::Salesdoc
     class ListService < CorevistAPI::Services::BaseServiceWithForm
+      include CorevistAPI::Services::Paginatable
+      include CorevistAPI::Services::Sortable
 
+      def call
+        perform!
+      end
+
+      private
+
+      def perform!
+        @rfc_result = rfc_service_for(:salesdoc_list, @form, @params).call
+
+        array = filter_by_query(@rfc_result.data)
+        array = sort_by_param(array)
+
+        invoices = paginate(salesdocs: array)
+        result(invoices)
+      end
     end
   end
 end
