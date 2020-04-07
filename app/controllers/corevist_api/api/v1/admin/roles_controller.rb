b395a274b2592b3882348bc24e862f85b85ad561
Yury Matusevich <yury.a.matusevich@gmail.com>
2020-04-08 00:25:20 +0300

    add roles list webservices

--- app/controllers/corevist_api/api/v1/admin/roles_controller.rb
+++ app/controllers/corevist_api/api/v1/admin/roles_controller.rb
@@ -1,6 +1,7 @@
 module CorevistAPI
   class API::V1::Admin::RolesController < API::V1::Admin::BaseController
     form_performer_for :index, :show, :create, :update, :destroy
+    configs_for :index
 
     private
 
