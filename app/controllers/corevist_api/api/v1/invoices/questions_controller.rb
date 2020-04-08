d86ffc2a72f611da99fef74fdcf9a3e47a7df47d
Yury Matusevich <yury.a.matusevich@gmail.com>
2020-04-09 00:34:11 +0300

    add have a question configs

---
+++ app/controllers/corevist_api/api/v1/invoices/questions_controller.rb
@@ -0,0 +1,5 @@
+module CorevistAPI
+  class API::V1::Invoices::QuestionsController < API::V1::BaseController
+    configs_for :new
+  end
+end
---
+++ app/controllers/corevist_api/api/v1/salesdocs/questions_controller.rb
@@ -0,0 +1,5 @@
+module CorevistAPI
+  class API::V1::Salesdocs::QuestionsController < API::V1::BaseController
+    configs_for :new
+  end
+end
