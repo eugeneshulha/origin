9bd4e581fd31acab038f41ece192321ffd6ac99c
Yury Matusevich <yury.a.matusevich@gmail.com>
2020-04-14 23:52:20 +0300

    fix name of component for first step 1

--- app/forms/corevist_api/forms/admin/users/step1.rb
+++ app/forms/corevist_api/forms/admin/users/step1.rb
@@ -5,7 +5,7 @@ module CorevistAPI
   module Forms::Admin
     module Users
       class Step1 < CorevistAPI::Forms::BaseForm
-        validate_component :admin_create_user_step_1_form, on_page: :admin_users_create_step_1
+        validate_component :admin_create_new_user_step_1, on_page: :admin_users_create_step_1
 
         def params_key
           'user'
