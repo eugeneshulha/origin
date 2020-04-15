1ca1a1e954b4c720cdba10de25aca203a6d98115
Yury Matusevich <yury.a.matusevich@gmail.com>
2020-04-15 15:14:08 +0300

    fix name of the file for step 3

--- app/forms/corevist_api/forms/admin/users/step3.rb
+++ app/forms/corevist_api/forms/admin/users/step3.rb
@@ -4,7 +4,7 @@
 module CorevistAPI
   module Forms
     class Admin::Users::Step3 < CorevistAPI::Forms::BaseForm
-      validate_component :roles_can_assign_of_new_user_form, on_page: :admin_users_new_step_3
+      validate_component :roles_can_assign_of_new_user_form, on_page: :admin_users_create_step_3
     end
   end
 end
