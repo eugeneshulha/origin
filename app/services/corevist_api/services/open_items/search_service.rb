62b55f64c11775c77c39f7d1baf4631b6fa495ad
Oleg Ryzhko <ryz.oleg@gmail.com>
2020-05-14 19:04:48 +0300

    add sorting and query search to open items

--- app/services/corevist_api/services/open_items/search_service.rb
+++ app/services/corevist_api/services/open_items/search_service.rb
@@ -1,15 +1,18 @@
 module CorevistAPI
   module Services
-    class OpenItems::SearchService< CorevistAPI::Services::BaseServiceWithForm
-      include Paginatable
+    class OpenItems::SearchService < CorevistAPI::Services::BaseServiceWithForm
 
       private
 
       def perform
-        rfc_result = rfc_service_for(:open_items, @form, @params).call
-        open_items = paginate(items: rfc_result.data[:open_items].flatten)
+        @rfc_result = rfc_service_for(:open_items, @form, @params).call
 
-        result(open_items)
+        open_items = @rfc_result.data[:open_items].flatten
+        array = sort_by_param(
+          filter_by_query(open_items)
+        )
+
+        result(paginate(items: array))
       end
     end
   end
