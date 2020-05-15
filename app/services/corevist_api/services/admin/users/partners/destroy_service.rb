ed6f12d4d3f85b1559cd5b02ca0e8c752ca5abb6
Oleg Ryzhko <ryz.oleg@gmail.com>
2020-05-15 14:09:40 +0300

    fix assigned partner deletion

--- app/services/corevist_api/services/admin/users/partners/destroy_service.rb
+++ app/services/corevist_api/services/admin/users/partners/destroy_service.rb
@@ -9,13 +9,7 @@ module CorevistAPI
             u = CorevistAPI::User.find_by(uuid: @form.user_uuid)
             raise CorevistAPI::ServiceException.new('api.errors.users.not_found') unless u
 
-            partner = u.partners.find_by(
-                id: @form.id,
-                function: @form.function,
-                number: @form.number,
-                sales_area_id: @form.sales_area_id,
-                assigned: true
-            )
+            partner = u.partners.find_by(id: @form.id, assigned: true)
             raise CorevistAPI::ServiceException.new('api.errors.users.partners.not_found') unless partner
 
             partner.destroy
