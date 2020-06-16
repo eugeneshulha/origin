eafb1d21ecf4c40f07e4a5455659f30973802b91
Yury Matusevich <yury.a.matusevich@gmail.com>
2020-06-16 12:54:53 +0300

    add edit configs to sap downtimes

--- app/forms/corevist_api/forms/admin/system_settings/sap_maintenance/sap_downtimes/update_form.rb
+++ app/forms/corevist_api/forms/admin/system_settings/sap_maintenance/sap_downtimes/update_form.rb
@@ -1,7 +1,7 @@
 module CorevistAPI
   module Forms::Admin::SystemSettings::SAPMaintenance::SAPDowntimes
     class UpdateForm < CorevistAPI::Forms::BaseForm
-      # validate_component :sales_area_update_form, on_page: :admin_system_settings_sales_areas_update
+      validate_component :sap_downtime_edit_form, on_page: :admin_system_settings_sap_maintenance_sap_downtimes_edit
     end
   end
 end
