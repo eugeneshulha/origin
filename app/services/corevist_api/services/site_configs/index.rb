53120d63b6e4b746123c67bd3aab4db8a7aededa
Yury Matusevich <yury.a.matusevich@gmail.com>
2020-06-19 16:39:22 +0300

    add user classifications to configs

--- app/services/corevist_api/services/site_configs/index.rb
+++ app/services/corevist_api/services/site_configs/index.rb
@@ -11,6 +11,7 @@ module CorevistAPI::Services::SiteConfigs
       hash = {
         microsites: CorevistAPI::Microsite.all.inject([]) { |memo, el| memo << { text: el.name, value: el.id }},
         user_types: CorevistAPI::UserType.all.inject([]) { |memo, el| memo << { text: el.title, value: el.value }},
+        user_classifications: CorevistAPI::UserClassification.all.inject([]) { |memo, el| memo << { text: el.title, value: el.title }},
         locales: Settings.locales.inject([]) { |memo, el| memo << { text: el, value: el }},
         sales_areas: CorevistAPI::SalesArea.all.inject([]) { |memo, el| memo << { text: el.title, value: el.title }},
         timezones: Settings.timezones.inject([]) { |memo, el| memo << { text: el[0], value: el[1] }},
