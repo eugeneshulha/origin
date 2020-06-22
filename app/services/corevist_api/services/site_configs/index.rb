eecd6067caef4e05f98d8f00aaa08660c67f7ebe
Yury Matusevich <yury.a.matusevich@gmail.com>
2020-06-22 12:15:43 +0300

    modify payments options

--- app/services/corevist_api/services/site_configs/index.rb
+++ app/services/corevist_api/services/site_configs/index.rb
@@ -10,7 +10,7 @@ module CorevistAPI::Services::SiteConfigs
     def perform
       hash = {
         microsites: CorevistAPI::Microsite.all.inject([]) { |memo, el| memo << { text: el.name, value: el.id }},
-        user_types: CorevistAPI::UserType.all.inject([]) { |memo, el| memo << { text: el.title, value: el.value }},
+        user_types: CorevistAPI::UserType.all.inject([]) { |memo, el| memo << { text: el.title, value: el.id }},
         user_classifications: CorevistAPI::UserClassification.all.inject([]) { |memo, el| memo << { text: el.id, value: el.id }},
         locales: Settings.locales.inject([]) { |memo, el| memo << { text: el, value: el }},
         sales_areas: CorevistAPI::SalesArea.all.inject([]) { |memo, el| memo << { text: el.title, value: el.title }},
