406143e2ec08786c84c88b95b1ede1a55e8e227b
Yury Matusevich <yury.a.matusevich@gmail.com>
2020-06-23 19:27:28 +0300

    fix registrations form

--- app/forms/corevist_api/forms/user/registration.rb
+++ app/forms/corevist_api/forms/user/registration.rb
@@ -1,4 +1,9 @@
 module CorevistAPI
   class Forms::User::Registration < CorevistAPI::Forms::BaseForm
+    validate_component :registration_form, on_page: :registrations_new
+
+    def params_key
+      :user
+    end
   end
 end
