09cc94d93cc981a2a7885b63ccbffd176a8f14e3
Yury Matusevich <yury.a.matusevich@gmail.com>
2020-06-23 19:42:09 +0300

    fix reset password link

--- app/helpers/corevist_api/devise_mailer_helper.rb
+++ app/helpers/corevist_api/devise_mailer_helper.rb
@@ -1,7 +1,7 @@
 module CorevistAPI
   module DeviseMailerHelper
     def react_edit_password_url(resource, options = {})
-      "#{Settings.self_url}/forgot_password/reset?reset_password_token=#{options[:reset_password_token]}"
+      "#{Settings.self_url}/password/reset?reset_password_token=#{options[:reset_password_token]}"
     end
   end
 end
