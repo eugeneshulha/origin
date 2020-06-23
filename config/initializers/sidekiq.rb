09ab943ab478dc1fc2f753f2b13534a986fa7d7a
Yury Matusevich <yury.a.matusevich@gmail.com>
2020-06-23 19:33:45 +0300

    disable sidekiq in development

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,9 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://172.16.100.11:6408/12' }
-end
+unless Rails.env.dev?
+  Sidekiq.configure_server do |config|
+    config.redis = {url: 'redis://172.16.100.11:6408/12'}
+  end
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://172.16.100.11:6408/12' }
+  Sidekiq.configure_client do |config|
+    config.redis = {url: 'redis://172.16.100.11:6408/12'}
+  end
 end
