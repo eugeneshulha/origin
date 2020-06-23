e0b9a375d0a1a459474c3b1870aeba8c2836eeb2
Yury Matusevich <yury.a.matusevich@gmail.com>
2020-06-23 10:15:35 +0300

    rename config file for open items create

--- app/forms/corevist_api/forms/open_items/create_form.rb
+++ app/forms/corevist_api/forms/open_items/create_form.rb
@@ -2,7 +2,7 @@ module CorevistAPI::Forms::OpenItems
   class CreateForm < CorevistAPI::Forms::BaseForm
     attr_accessor :credit_card, :auth_token, :comp_code, :payer_number, :valid_on
 
-    validate_component :create_payment_form, on_page: :payments_create
+    validate_component :create_payment_form, on_page: :open_items_create
 
 
     def pay_with_cc?
