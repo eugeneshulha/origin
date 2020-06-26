3f3d104dc0c3a3e1ead0bdff07cc22c071f8609c
Yury Matusevich <yury.a.matusevich@gmail.com>
2020-06-26 14:16:37 +0300

    beautify time output at downtimes

--- app/models/corevist_api/sap_downtime.rb
+++ app/models/corevist_api/sap_downtime.rb
@@ -12,5 +12,25 @@ module CorevistAPI
       time = Time.zone.now.to_time
       CorevistAPI::SAPDowntime.where(str, date: date, time: time).first
     end
+
+    def down_to_time
+      t_format = CorevistAPI::Context.current_user.time_format
+      self.read_attribute(:down_to_time)&.strftime(t_format)
+    end
+
+    def down_from_time
+      t_format = CorevistAPI::Context.current_user.time_format
+      self.read_attribute(:down_from_time)&.strftime(t_format)
+    end
+    #
+    # def down_to_date
+    #   d_format = CorevistAPI::Context.current_user.date_format
+    #   self.read_attribute(:down_to_date)&.strftime(d_format)
+    # end
+    #
+    # def down_from_date
+    #   d_format = CorevistAPI::Context.current_user.date_format
+    #   self.read_attribute(:down_from_date)&.strftime(d_format)
+    # end
   end
 end
