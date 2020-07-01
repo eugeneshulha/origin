93314bd5fcfc8322431a2db0215f939a697585c9
Yury Matusevich <yury.a.matusevich@gmail.com>
2020-07-01 18:14:12 +0300

    add 6th step to user create process, modify status endpoint

--- app/controllers/corevist_api/api/v1/statuses_controller.rb
+++ app/controllers/corevist_api/api/v1/statuses_controller.rb
@@ -1,9 +1,15 @@
 module CorevistAPI
   class API::V1::StatusesController < CorevistAPI::API::BaseController
     skip_before_action :authenticate_user!
+    skip_before_action :establish_sap_connection
 
     def status
-      render json: { status: :ok }, status: :ok
+      res = {
+          rails: :ok,
+          mysql: ActiveRecord::Base.connected? ? :ok : :not_ok,
+          sap: CorevistAPI::Context.current_connection&.ping ? :ok : :not_ok
+      }
+      render json: res, status: :ok
     end
   end
 end
