8ebb7106cc2950219095928825c8db39693e574a
Yury Matusevich <yury.a.matusevich@gmail.com>
2020-07-09 14:49:14 +0300

    fix labels for user formats

--- app/services/corevist_api/services/site_configs/index.rb
+++ app/services/corevist_api/services/site_configs/index.rb
@@ -7,12 +7,12 @@ module CorevistAPI::Services::SiteConfigs
         microsites: CorevistAPI::Microsite.all.inject([]) { |memo, el| memo << { text: el.name, value: el.id }},
         user_types: CorevistAPI::UserType.all.inject([]) { |memo, el| memo << { text: el.title, value: el.id }},
         user_classifications: CorevistAPI::UserClassification.all.inject([]) { |memo, el| memo << { text: el.id, value: el.id }},
-        locales: Settings.locales.inject([]) { |memo, el| memo << { text: el, value: el }},
+        locales: Settings.locales.inject([]) { |memo, el| memo << { text: _("lbl|locale #{el}"), value: el }},
         sales_areas: CorevistAPI::SalesArea.all.inject([]) { |memo, el| memo << { text: el.title, value: el.title }},
         timezones: Settings.timezones.inject([]) { |memo, el| memo << { text: el[0], value: el[1] }},
-        number_formats: Settings.number_formats.inject([]) { |memo, el| memo << { text: el[0], value: el[1] }},
-        date_formats: Settings.date_formats.inject([]) { |memo, el| memo << { text: el[0], value: el[1] }},
-        time_formats: Settings.time_formats.inject([]) { |memo, el| memo << { text: el[0], value: el[1] }},
+        number_formats: Settings.number_formats.inject([]) { |memo, el| memo << { text: _("lbl|number format #{el[0]}"), value: el[1] }},
+        date_formats: Settings.date_formats.inject([]) { |memo, el| memo << { text: _("lbl|date format #{el[0]}"), value: el[1] }},
+        time_formats: Settings.time_formats.inject([]) { |memo, el| memo << { text: _("lbl|time format #{el[0]}"), value: el[1] }},
       }
 
       result(hash)
