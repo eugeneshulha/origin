843a920dd09375983eefa4537d2d3c05b846271d
Yury Matusevich <yury.a.matusevich@gmail.com>
2020-07-10 14:25:55 +0300

    show payments data with format

--- app/models/corevist_api/payment.rb
+++ app/models/corevist_api/payment.rb
@@ -2,18 +2,29 @@ require File.join(CorevistAPI::Engine.root, 'app/models/corevist_api/payment/ite
 
 module CorevistAPI
   class Payment
+    include CorevistAPI::FormatConversion
+
     attr_accessor :amount, :amount_in_doc_currency, :assignment_number, :cc_number, :cc_type, :currency,
                   :debit_or_credit, :description, :doc_currency, :fiscal_type, :item_text, :fiscal_year,
                   :payment_number, :payment_date, :reason_code
     # arrays
     attr_accessor :items
 
+    format_date :payment_date
+    format_amount :amount, :amount_in_doc_currency
+
     def initialize
       @items = []
     end
 
     def as_json
-      super.except("items").merge({ invoices: items.map(&:invoice_number) })
+      hash = instance_variables.inject({}) do |memo, var|
+        v = var.to_s.tr("@", '')
+        memo[v] = self.send(v)
+        memo
+      end
+
+      hash.except("items").merge({ invoices: items.map(&:invoice_number) })
     end
   end
 end
